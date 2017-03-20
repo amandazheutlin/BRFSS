6bc6b1d9479cd74658957a1cfdfacaa981a6f54a
Tuan Nguyen Doan <tuandoan.nguyen@yale.edu>
2017-03-20 15:09:21 -0400

    fixed string problem in description

--- decode_variable.R
+++ decode_variable.R
@@ -1,24 +1,32 @@
 library (pdftools)
 
+##########################
+#get variable description#
+
 text <- pdf_text("./R/codebook.pdf")
 
 name <- vector(length =0)
 meaning<- vector (length =0)
 
-# loop through every page
+
 for (i in 2: length(text)){
+  # get variable names from each page
   lines <- strsplit(text[i], "\n") %>% unlist # extract every lines
   name_str <- lines[which(grepl("SAS Variable Name",lines))] # search for lines of variables name
-  variable_name <- lapply(name_str, function (k) unlist(strsplit(k, "SAS Variable Name: "))[2])
-  description_str <- lines[which(grepl("Description",lines))] # search for lines of variable description
-  description <- lapply (description_str, function (k) unlist(strsplit(k, "Description: "))[2])
-
-  name <- append(name, unlist(variable_name))
-  meaning <- append(meaning, unlist(description))
+  variable_name <- lapply(name_str, function (k) unlist(strsplit(k, "SAS Variable Name: "))[2]) %>% unlist
+  
+  # get description name from each plage
+  lines <- strsplit(text[i], "Weighted") %>% unlist
+  description <- lapply (lines, function (k) unlist(strsplit(k, "Description: "))[2]) %>% unlist %>% na.omit
+  
+  name <- append(name, variable_name)
+  meaning <- append(meaning, description)
 }
 
 
 name <- unlist(lapply (name, function (i) ifelse (substring(i,1,1) == "_", gsub("_", "X_",i),i)))
-code_book <- data.frame(name, meaning, stringsAsFactors = FALSE)
+code_book <- data.frame(variable = name, description = meaning, stringsAsFactors = FALSE)
+
+write.csv(code_book, "./R/variable.csv", row.names = FALSE)
 
 
