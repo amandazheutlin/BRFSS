7f80009b367232ba43983854fed55bfa6c6c4ccd
Tuan Nguyen Doan <tuandoan.nguyen@yale.edu>
2017-03-25 11:56:23 -0400

    Update elastic_net.R
    Fixed Penalized logistic regression, VarImp. Add confusionMatrix, Lift and Calibration

--- elastic_net.R
+++ elastic_net.R
@@ -5,6 +5,7 @@ registerDoMC(cores = detectCores())
 
 source("~/R/helpers.R")
 
+find_me("MENTHLTH")
 find_me ("X_RFBING5")
 brfss2015 <- read.xport("~/R/LLCP2015.XPT ")
 brfss2013 <- read.xport("~/R/LLCP2013.XPT")
@@ -29,6 +30,7 @@ enet       <- train(x= as.matrix(pred), y = as.numeric(depression_2013$outcome),
                     metric = "Rsquared",
                     #tuneGrid = eGrid,
                     trControl = fitControl)
+enet
 plot(enet)
 enet$bestTune
 
@@ -38,74 +40,108 @@ var_imp <- var_important (enet,10)
 # draw a chart of importance:
 chart(var_imp, "depression")
 
+# Another way to draw the chart of varImp:
+plot <- ggplot(varImp(enet)) + 
+        theme_bw() + 
+        ggtitle("Variable Importance", subtitle = "elastic net on depression") + 
+        theme(panel.border = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major.y = element_blank())
+
 # apply best tune alpha and lambda to fit on brfss2015
 predict_2015 <- predict(enet, newdata = as.matrix(depression_2015[,-which(names(depression_2015)=="outcome")]), type ="raw")
 mean((predict_2015-depression_2015$outcome)^2)
-# MSE of test set = 0.2, not too bad
 
-################################
-# Elastic net on binge drinking#
-################################
-binge_data_2013 <- clean_data_binge(brfss2013)
-pred2    <- binge_data_2013 %>% dplyr::select(EXERANY2:X_PASTAE1, -X_RFBING5) #exclude X_RFBING5
-outcome2 <- binge_data_2013[,"X_RFBING5"]
-fitControl  <- trainControl(method = "cv", savePredictions = TRUE)
-eGrid       <- expand.grid(.alpha = seq(0,1,by=0.05), .lambda = seq(0,1,by=0.05))
-enet2       <- train(x= as.matrix(pred2), y = as.numeric(outcome2),
-                     method = "glmnet", 
-                     metric = "Rsquared",
-                     #tuneGrid = eGrid, it seems that R has troubled fitting the model using this grid
-                     #probably some linear algebra problem under the hood
-                     trControl = fitControl)
-plot(enet2)
-enet2$bestTune
 
-var_imp <- var_important (enet2,10)
-chart(var_imp, "binge")
+########################################
+# Logistic with Ridge and LASSO penalty#
+########################################
 
-# Use model 2013 to predict 2015
+### Clean data
 
 binge_data_2015 <- clean_data_binge(brfss2015)
-pred_2015 <- binge_data_2015 %>% dplyr::select(EXERANY2:X_PASTAE1, -X_RFBING5) 
-predict_2015 <- predict(enet2, newdata = as.matrix(pred_2015), type ="raw")
-mean((predict_2015-binge_data_2015$X_RFBING5)^2)
-
-##########################################
-# Multinomial Logistic with Ridge penalty#
-##########################################
-outcome2 <- as.factor(outcome2)
-multinom_ridge <- cv.glmnet (as.matrix(pred2), outcome2, alpha = 0, nfolds = 10, family = "multinomial", type.measure = "class")
-plot(multinom_ridge)               
-lambda_min <- multinom_ridge$lambda.min
-lambda_min
-# minimum lambda is 106.3541
-
-# Predict misclassification rate on 2015 data
-predict_2015 <- predict(multinom_ridge, s = lambda_min, newx = as.matrix(pred_2015), type = "class")
-mean(predict_2015 != binge_data_2015$X_RFBING5)
-# Misclassification rate is about 17.2649% not bad at all
+binge_data_2015 <- binge_data_2015[!binge_data_2015$X_RFBING5 == 9,]## omit Don't know/Refused/Missing
 
 
-############################################
-  # Multinomial Logistic with LASSO penalty#
-############################################
+binge_data_2013 <- clean_data_binge(brfss2013)
+binge_data_2013 <- binge_data_2013[!binge_data_2013$X_RFBING5 == 9,]## omit Don't know/Refused/Missing
 
-multinom_lasso <- cv.glmnet (as.matrix(pred2), outcome2, alpha = 1, nfolds = 10, family = "multinomial", type.measure = "class")
-plot(multinom_lasso)               
-lambda_min <- multinom_lasso$lambda.min
-lambda_min
-# lambda_min is 0.0007679625
 
-# Predict misclassification rate on 2015 data
-predict_2015 <- predict(multinom_lasso, s = lambda_min, newx = as.matrix(pred_2015), type = "class")
-mean(predict_2015 != binge_data_2015$X_RFBING5)
-# Misclassification rate is about 17.20% 
+################### doing by caret
+binge_data_2015$X_RFBING5 <- as.factor(binge_data_2015$X_RFBING5)
+binge_data_2013$X_RFBING5 <- as.factor(binge_data_2013$X_RFBING5)
 
-#Find cofficients at lambda_min
-# Find unique predictors across 3 multinomial logistic regression equations
-coef_lasso <- predict(multinom_lasso, s = lambda_min, type = "coefficients")
-coef_1<-rownames(coef_lasso$`1`)[which(!coef_lasso$`1` == 0)]
-coef_2<-rownames(coef_lasso$`2`)[which(!coef_lasso$`2` == 0)]
-coef_9<-rownames(coef_lasso$`9`)[which(!coef_lasso$`9` == 0)]
+summary(binge_data_2013$X_RFBING5)
+summary(binge_data_2015$X_RFBING5)
 
-unique(c(coef_1, coef_2, coef_9)) # 26 unique variables in this model
+glmnet_grid <- expand.grid(alpha = c(0,  .1,  .2, .4, .6, .8, 1),
+                           lambda = seq(.01, .2, length = 20))
+glmnet_ctrl <- trainControl(method = "cv", number = 10)
+glmnet_fit <- train(X_RFBING5 ~ ., data = binge_data_2013,
+                    method = "glmnet",
+                    #tuneGrid = glmnet_grid,
+                    trControl = glmnet_ctrl)
+glmnet_fit
+glmnet_fit$bestTune
+coef(glmnet_fit$finalModel, glmnet_fit$bestTune$alpha, glmnet_fit$bestTune$lambda)
+
+plot(glmnet_fit)
+
+varImp(glmnet_fit, scale = FALSE)
+ggplot(varImp(glmnet_fit, scale = FALSE)) + 
+  theme_bw() + 
+  ggtitle("Variable Importance", subtitle = "penalized logistic regression at best tune alpha, lambda") + 
+  theme(panel.border = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major.y = element_blank())
+
+
+pred_glm <- predict(glmnet_fit, new = binge_data_2015)
+confusionMatrix(data = pred_glm, reference = binge_data_2015$X_RFBING5)
+glm_probs <- predict(glmnet_fit, newdata = binge_data_2015, type = "prob")[,"1"]
+
+
+#############################
+# Normal logistic regression#
+
+logistic <- glm(X_RFBING5 ~., data = binge_data_2013, family = "binomial")
+pred_logistic <- predict(logistic, newdata = binge_data_2015, type ="response")
+contrasts(binge_data_2013$X_RFBING5) # find how R encode contrast, so 1 is encoded as 0, 2 as 1.
+pred_logistic <- factor(ifelse(pred_logistic>= 0.5, 2, 1))
+
+confusionMatrix(data = pred_logistic, reference = binge_data_2015$X_RFBING5)
+
+logistic_prob <- 1- predict(logistic, newdata = binge_data_2015, type ="response") # we want prob for class 1
+
+#logistic is not a glmnet object so we have to process VarImp manually
+varimp_logistic = cbind(variable = row.names (varImp(logistic, scale = TRUE)), varImp(logistic, scale = TRUE))
+varimp_logistic <- merge(varimp_logistic, code_book ,by = "variable") %>%arrange(-Overall)
+varimp_logistic <- varimp_logistic[1:10,]
+names(varimp_logistic) <- c("name", "Importance", "label")
+chart(varimp_logistic, "binge")
+
+
+###################################
+# Lift curve and calibration curve#
+
+result <- data.frame(Class = binge_data_2015$X_RFBING5)
+result$glm <- glm_probs
+result$logistic <- logistic_prob
+
+head(result)
+trellis.par.set(caretTheme())
+lift_obj <- lift(Class ~ glm + logistic, data = result, cuts =13)
+plot(lift_obj, values =c(30,60), auto.key = list(columns = 2,
+                                            lines = TRUE,
+                                            points = FALSE))
+
+
+trellis.par.set(caretTheme())
+cal_obj <- calibration(Class ~ glm+logistic,
+                       data = result,
+                       cuts = 13)
+plot(cal_obj, type = "l", auto.key = list(columns = 2,
+                                          lines = TRUE,
+                                          points = FALSE))
+ggplot(cal_obj)
+
